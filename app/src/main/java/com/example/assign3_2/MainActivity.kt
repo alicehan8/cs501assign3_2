5a3892ee2a7d0bee63460dbe2d7d85e4f87da3b3
alicehan8 <alicehan@bu.edu>
2025-09-30 00:29:48 -0400

    completed

--- app/src/main/java/com/example/assign3_2/MainActivity.kt
+++ app/src/main/java/com/example/assign3_2/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.assign3_2
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.assign3_2.ui.theme.Assign3_2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +38,62 @@ class MainActivity : ComponentActivity() {
         setContent {
             Assign3_2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ProfileCard(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+//a lot of code taken from class example
+@Composable
+fun ProfileCard(modifier: Modifier = Modifier){
+    var badge by remember { mutableStateOf(false) }
+    //outer profile card container
+    Box(
+        modifier = modifier
+            .size(width = 150.dp, height = 200.dp)
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colorScheme.surfaceVariant)
+            .border(1.dp, MaterialTheme.colorScheme.outlineVariant, MaterialTheme.shapes.medium)
+    ){
+        //profile picture circle container
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(16.dp)
+                .size(80.dp)
+                .clip(CircleShape)
+                .background(Color.DarkGray)
+                .border(2.dp, Color.White, CircleShape)
+        ) {
+            //initials for profile picture placeholder
+            Text("AH", color=Color.White, modifier = Modifier.align(Alignment.Center))
+            if(badge) {
+                //include notification badge on profile picture
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(10.dp)
+                        .size(24.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.error)
+                        .border(1.5.dp, Color.White, CircleShape),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "3",
+                        color = Color.White,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold
                     )
                 }
             }
+        }
+
+        //button to control the badge
+        Button(onClick = { badge = !badge }, modifier.align(Alignment.BottomCenter)){
+            Text(text = "Toggle Badge")
         }
     }
 }
